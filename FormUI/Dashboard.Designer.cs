2c695ac6e093ebfab589c50f0597c52b582f01df
unknown <tamaramoya@paucasesnovescifp.cat>
2020-11-20 08:35:48 +0100

    Initial commit

---
+++ FormUI/Dashboard.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace FormUI
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PeopleFoundListBox = new System.Windows.Forms.ListBox();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.lasNamelabel1 = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.firstNameInsLabel = new System.Windows.Forms.Label();
+            this.firstNameInsText = new System.Windows.Forms.TextBox();
+            this.lastNameInsLabel = new System.Windows.Forms.Label();
+            this.lastNameInsText = new System.Windows.Forms.TextBox();
+            this.emailInsLabel = new System.Windows.Forms.Label();
+            this.emailInstText = new System.Windows.Forms.TextBox();
+            this.phoneNumberInsLabel = new System.Windows.Forms.Label();
+            this.phoneNumberInstText = new System.Windows.Forms.TextBox();
+            this.insertRecordButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PeopleFoundListBox
+            // 
+            this.PeopleFoundListBox.FormattingEnabled = true;
+            this.PeopleFoundListBox.ItemHeight = 22;
+            this.PeopleFoundListBox.Location = new System.Drawing.Point(36, 186);
+            this.PeopleFoundListBox.Name = "PeopleFoundListBox";
+            this.PeopleFoundListBox.Size = new System.Drawing.Size(500, 136);
+            this.PeopleFoundListBox.TabIndex = 0;
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(165, 52);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(192, 28);
+            this.lastNameText.TabIndex = 1;
+            // 
+            // lasNamelabel1
+            // 
+            this.lasNamelabel1.AutoSize = true;
+            this.lasNamelabel1.Location = new System.Drawing.Point(31, 52);
+            this.lasNamelabel1.Name = "lasNamelabel1";
+            this.lasNamelabel1.Size = new System.Drawing.Size(99, 24);
+            this.lasNamelabel1.TabIndex = 2;
+            this.lasNamelabel1.Text = "Last Name";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(165, 110);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(106, 38);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // firstNameInsLabel
+            // 
+            this.firstNameInsLabel.AutoSize = true;
+            this.firstNameInsLabel.Location = new System.Drawing.Point(31, 337);
+            this.firstNameInsLabel.Name = "firstNameInsLabel";
+            this.firstNameInsLabel.Size = new System.Drawing.Size(92, 24);
+            this.firstNameInsLabel.TabIndex = 5;
+            this.firstNameInsLabel.Text = "Firt Name";
+            // 
+            // firstNameInsText
+            // 
+            this.firstNameInsText.Location = new System.Drawing.Point(165, 337);
+            this.firstNameInsText.Name = "firstNameInsText";
+            this.firstNameInsText.Size = new System.Drawing.Size(192, 28);
+            this.firstNameInsText.TabIndex = 4;
+            // 
+            // lastNameInsLabel
+            // 
+            this.lastNameInsLabel.AutoSize = true;
+            this.lastNameInsLabel.Location = new System.Drawing.Point(31, 371);
+            this.lastNameInsLabel.Name = "lastNameInsLabel";
+            this.lastNameInsLabel.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lastNameInsLabel.Size = new System.Drawing.Size(99, 24);
+            this.lastNameInsLabel.TabIndex = 7;
+            this.lastNameInsLabel.Text = "Last Name";
+            // 
+            // lastNameInsText
+            // 
+            this.lastNameInsText.Location = new System.Drawing.Point(165, 371);
+            this.lastNameInsText.Name = "lastNameInsText";
+            this.lastNameInsText.Size = new System.Drawing.Size(192, 28);
+            this.lastNameInsText.TabIndex = 6;
+            // 
+            // emailInsLabel
+            // 
+            this.emailInsLabel.AutoSize = true;
+            this.emailInsLabel.Location = new System.Drawing.Point(31, 405);
+            this.emailInsLabel.Name = "emailInsLabel";
+            this.emailInsLabel.Size = new System.Drawing.Size(57, 24);
+            this.emailInsLabel.TabIndex = 9;
+            this.emailInsLabel.Text = "Email";
+            // 
+            // emailInstText
+            // 
+            this.emailInstText.Location = new System.Drawing.Point(165, 405);
+            this.emailInstText.Name = "emailInstText";
+            this.emailInstText.Size = new System.Drawing.Size(192, 28);
+            this.emailInstText.TabIndex = 8;
+            // 
+            // phoneNumberInsLabel
+            // 
+            this.phoneNumberInsLabel.AutoSize = true;
+            this.phoneNumberInsLabel.Location = new System.Drawing.Point(31, 439);
+            this.phoneNumberInsLabel.Name = "phoneNumberInsLabel";
+            this.phoneNumberInsLabel.Size = new System.Drawing.Size(66, 24);
+            this.phoneNumberInsLabel.TabIndex = 11;
+            this.phoneNumberInsLabel.Text = "Phone";
+            // 
+            // phoneNumberInstText
+            // 
+            this.phoneNumberInstText.Location = new System.Drawing.Point(165, 439);
+            this.phoneNumberInstText.Name = "phoneNumberInstText";
+            this.phoneNumberInstText.Size = new System.Drawing.Size(192, 28);
+            this.phoneNumberInstText.TabIndex = 10;
+            // 
+            // insertRecordButton
+            // 
+            this.insertRecordButton.Location = new System.Drawing.Point(165, 497);
+            this.insertRecordButton.Name = "insertRecordButton";
+            this.insertRecordButton.Size = new System.Drawing.Size(106, 38);
+            this.insertRecordButton.TabIndex = 12;
+            this.insertRecordButton.Text = "Insert";
+            this.insertRecordButton.UseVisualStyleBackColor = true;
+            this.insertRecordButton.Click += new System.EventHandler(this.insertRecordButton_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.insertRecordButton);
+            this.Controls.Add(this.phoneNumberInsLabel);
+            this.Controls.Add(this.phoneNumberInstText);
+            this.Controls.Add(this.emailInsLabel);
+            this.Controls.Add(this.emailInstText);
+            this.Controls.Add(this.lastNameInsLabel);
+            this.Controls.Add(this.lastNameInsText);
+            this.Controls.Add(this.firstNameInsLabel);
+            this.Controls.Add(this.firstNameInsText);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.lasNamelabel1);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.PeopleFoundListBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Dashboard";
+            this.Text = "SQL Data Acces Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox PeopleFoundListBox;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.Label lasNamelabel1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label firstNameInsLabel;
+        private System.Windows.Forms.TextBox firstNameInsText;
+        private System.Windows.Forms.Label lastNameInsLabel;
+        private System.Windows.Forms.TextBox lastNameInsText;
+        private System.Windows.Forms.Label emailInsLabel;
+        private System.Windows.Forms.TextBox emailInstText;
+        private System.Windows.Forms.Label phoneNumberInsLabel;
+        private System.Windows.Forms.TextBox phoneNumberInstText;
+        private System.Windows.Forms.Button insertRecordButton;
+    }
+}
+
---
+++ FormUI/Dashboard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormUI
+{
+    public partial class Dashboard : Form
+    {
+        List<Person> people = new List<Person>();
+        public Dashboard()
+        {
+            InitializeComponent();
+            UpdateBinding();
+        }
+
+        private void UpdateBinding()
+        {
+            PeopleFoundListBox.DataSource = people;
+            PeopleFoundListBox.DisplayMember = "FullInfo";
+        }
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            DataAcces db = new DataAcces();
+            people= db.GetPeople(lastNameText.Text);
+
+            UpdateBinding();
+        }
+
+        private void insertRecordButton_Click(object sender, EventArgs e)
+        {
+            DataAcces db = new DataAcces();
+
+            db.InsertPerson(firstNameInsText.Text, lastNameInsText.Text, emailInstText.Text, phoneNumberInstText.Text);
+
+            firstNameInsText.Text = "";
+            lastNameInsText.Text = "";
+            emailInstText.Text = "";
+            phoneNumberInstText.Text = "";
+        }
+    }
+}
